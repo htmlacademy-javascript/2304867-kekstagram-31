57e169741c82d444eaafd34f8bb9466773f149eb
askaryaminov <askaryaminov9@gmail.com>
2024-03-26 21:10:53 +0400

    В процессе выполнения 7-го модуля

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { initArrayBuilder } from './array-builder';
+import { generatePictures } from './thumbnails';
 
 //const photos = initArrayBuilder();
 console.log(initArrayBuilder());
-
+//initArrayBuilder();
+//generatePictures(initArrayBuilder());
